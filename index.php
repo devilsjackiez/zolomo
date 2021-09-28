0d48385b83bccd0c9a227c6666cf1be5b80d6c57
JACKRIN PATANASUTAKIT <jackrin_pat@cmu.ac.th>
2021-09-28 10:34:28 +0700

    Update ZOLOMO

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Zolomo Web Application</title>
+    <title>Zolomo Web Application Test</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
